a8538625c05ee100d3e3de1d9c98dce18fc96a9d
keyongjun <51976632+keyongjun@users.noreply.github.com>
2021-08-12 17:47:15 +0900

    WishlistDAO getString 추가

--- step13_smallproject/src/interior/model/WishlistDAO.java
+++ step13_smallproject/src/interior/model/WishlistDAO.java
@@ -47,7 +47,7 @@ public class WishlistDAO {
 		pstmt.setInt(1, wishlistNum);
 		rset = pstmt.executeQuery();
 		if (rset.next()) {
-			wishlist = new WishlistDTO(rset.getInt(1), rset.getInt(2));
+			wishlist = new WishlistDTO(rset.getInt(1), rset.getInt(2), rset.getString(3));
 		}
 	} finally {
 		DBUtil.close(con, pstmt, rset);
@@ -73,7 +73,7 @@ public class WishlistDAO {
 		return false;
 	}
 	
-	// pnumÀ¸·Î ÇØ´ç Á¦Ç° ¸ðµç Á¤º¸ ¹ÝÈ¯
+	// pnumìœ¼ë¡œ í•´ë‹¹ ì œí’ˆ ëª¨ë“  ì •ë³´ ë°˜í™˜
 	public static ArrayList<WishlistDTO> getAllWishlist() throws SQLException {
 		Connection con = null;
 		PreparedStatement pstmt = null;
@@ -86,7 +86,7 @@ public class WishlistDAO {
 
 			list = new ArrayList<WishlistDTO>();
 			while (rset.next()) {
-				list.add(new WishlistDTO(rset.getInt(1), rset.getInt(2)));
+				list.add(new WishlistDTO(rset.getInt(1), rset.getInt(2), rset.getString(3)));
 			}
 		} finally {
 			DBUtil.close(con, pstmt, rset);
@@ -95,133 +95,3 @@ public class WishlistDAO {
 	}
 	
 }
-
-
-
-//package interior.model;
-//
-//import java.sql.Connection;
-//
-//import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.util.ArrayList;
-//import java.util.Properties;
-//
-//import interior.model.dto.DecorationDTO;
-//import interior.model.util.DBUtil;
-//
-//
-//public class DecorationDAO {
-//	
-//	private static Properties sql = DBUtil.getSql();
-//
-//	
-//	public static boolean addDecoration(DecorationDTO decoration) throws SQLException {
-//		Connection con = null;
-//		PreparedStatement pstmt = null;
-//		try {
-//			con = DBUtil.getConnection();
-//			
-//			pstmt = con.prepareStatement("insert into decoration values(?, ?, ?, ?, ?, ?");
-//
-//			pstmt.setInt(1, decoration.getPnum());
-//			pstmt.setString(2, decoration.getDecorationPname());
-//			pstmt.setInt(3, decoration.getDecorationPrice());
-//			pstmt.setString(4, decoration.getDecorationStandard());
-//			pstmt.setString(5, decoration.getDecorationStandard());
-//			pstmt.setInt(6, decoration.getDecorationPriority());
-//
-//			int result = pstmt.executeUpdate();
-//
-//			if (result == 1) {
-//				return true;
-//			}
-//		} finally {
-//			DBUtil.close(con, pstmt);
-//		}
-//		return false;
-//	}
-//	
-//	//  update decoration set ¿­ = ? where pnum =? ÀÎµ¥ ¾ÆÁ÷ º¸·ù ! ¼öÁ¤ ÇÊ¿ä
-//	public static boolean updateDecoration(int decorationNum, String major) throws SQLException {
-//		Connection con = null;
-//		PreparedStatement pstmt = null;
-//		try {
-//			con = DBUtil.getConnection();
-//
-//			pstmt = con.prepareStatement(sql.getProperty("updateDecoration"));
-//			pstmt.setString(1, major);
-//			pstmt.setInt(2, decorationNum);
-//
-//			int result = pstmt.executeUpdate();
-//			if (result == 1) {
-//				return true;
-//			}
-//		} finally {
-//			DBUtil.close(con, pstmt);
-//		}
-//		return false;
-//	}
-//	
-//	public static DecorationDTO getDecoration(int decorationNum) throws SQLException {
-//		Connection con = null;
-//		PreparedStatement pstmt = null;
-//		ResultSet rset = null;
-//		DecorationDTO decoration = null;
-//
-//		try {
-//			con = DBUtil.getConnection();
-//			pstmt = con.prepareStatement("select * from decoration where decorationNum=?");
-//			pstmt.setInt(1, decorationNum);
-//			rset = pstmt.executeQuery();
-//			if (rset.next()) {
-//				decoration = new DecorationDTO(rset.getInt(1), rset.getString(2), rset.getInt(3), rset.getString(4), rset.getString(5), rset.getInt(6)
-//);
-//			}
-//		} finally {
-//			DBUtil.close(con, pstmt, rset);
-//		}
-//		return decoration;
-//	}
-//	
-//	
-//	// sql - delete from activist where activist_id=?
-//	public static boolean deleteDecoration(int decorationNum) throws SQLException {
-//		Connection con = null;
-//		PreparedStatement pstmt = null;
-//		try {
-//			con = DBUtil.getConnection();
-//			pstmt = con.prepareStatement("delete from decoration where decorationNum=?");
-//			pstmt.setInt(1, decorationNum);
-//			int result = pstmt.executeUpdate();
-//			if (result == 1) {
-//				return true;
-//			}
-//		} finally {
-//			DBUtil.close(con, pstmt);
-//		}
-//		return false;
-//	}
-//	
-//	// pnumÀ¸·Î ÇØ´ç Á¦Ç° ¸ðµç Á¤º¸ ¹ÝÈ¯
-//	public static ArrayList<DecorationDTO> getAllDecoration() throws SQLException {
-//		Connection con = null;
-//		PreparedStatement pstmt = null;
-//		ResultSet rset = null;
-//		ArrayList<DecorationDTO> list = null;
-//		try {
-//			con = DBUtil.getConnection();
-//			pstmt = con.prepareStatement(sql.getProperty("getAllActivists"));
-//			rset = pstmt.executeQuery();
-//
-//			list = new ArrayList<DecorationDTO>();
-//			while (rset.next()) {
-//				list.add(new DecorationDTO(rset.getInt(1), rset.getString(2), rset.getInt(3), rset.getString(4), rset.getString(5), rset.getInt(6)));
-//			}
-//		} finally {
-//			DBUtil.close(con, pstmt, rset);
-//		}
-//		return list;
-//	}
-//}
